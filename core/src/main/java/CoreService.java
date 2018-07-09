7deda1c0728352501d99e5549ee4206d53aa1ef8
ChathuminaVimukthi <chathumina.2016238@iit.ac.lk>
2018-07-09 14:38:45 +0530

    Print Method in Core

--- core/src/main/java/CoreService.java
+++ core/src/main/java/CoreService.java
@@ -3,4 +3,10 @@ public class CoreService {
     public void CoreService(){
         System.out.println("From core services");
     }
+
+    public void PrintSome(){
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
 }
